78e2684121715a57d16e85dada3829b263e42ef4
LeGusto <janusonis.gustas@gmail.com>
2024-11-25 11:47:06 +0200

    Deleted unused file

--- src/GeneralAlgorithms/BinarySearch.tsx
+++ src/GeneralAlgorithms/BinarySearch.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./BinarySearch.css";
 import "../Templates/ReusableCSS.css";
 
@@ -6,7 +6,7 @@ interface Props {
   changeMenu: (name: string) => void;
 }
 
-const BinarySearch = (_props: Props) => {
+const BinarySearch = (props: Props) => {
   // Entries in array
   var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
   const searchVal = 2;
@@ -20,7 +20,7 @@ const BinarySearch = (_props: Props) => {
     numArray.length - 1
   );
   const [NextStep, setNextStep] = useState("Middle value");
-  const [CurrentPhase, setCurrentPhase] = useState("Start");
+  const [isFading, setIsFading] = useState(false);
 
   // Refs
   const arrayRefs = useRef<(HTMLDivElement | null)[]>([]);
@@ -28,13 +28,12 @@ const BinarySearch = (_props: Props) => {
   const leftPointerRef = useRef<HTMLDivElement | null>(null);
   const rightPointerRef = useRef<HTMLDivElement | null>(null);
   const nextButtonRef = useRef<HTMLButtonElement | null>(null);
+  const mainContainerRef = useRef<HTMLDivElement | null>(null);
 
   const fixPointer = (
     pointer: HTMLDivElement | null,
     item: HTMLDivElement | null
   ) => {
-    //useCallback
-
     if (pointer) {
       if (item) {
         // Get absolute position of first element in array
@@ -55,6 +54,8 @@ const BinarySearch = (_props: Props) => {
 
   // Find next pointer boundaries using BS
   const handleNextClick = () => {
+    if (isFading) return;
+
     const mid =
       LeftPointerIndex + Math.floor((RightPointerIndex - LeftPointerIndex) / 2);
 
@@ -82,6 +83,35 @@ const BinarySearch = (_props: Props) => {
     } else console.log("Done");
   };
 
+  const handleReturnClick = () => {
+    // Already clicked
+    if (isFading) return;
+
+    setIsFading(true);
+
+    if (mainContainerRef) {
+      const handleAnimationEnd = (event: { animationName: string }) => {
+        if (event.animationName != "fade") return;
+
+        // Remove the event listener after it fires
+        mainContainerRef.current!.removeEventListener(
+          "animationend",
+          handleAnimationEnd
+        );
+
+        // Initialize main menu
+        props.changeMenu("back");
+      };
+
+      mainContainerRef.current?.addEventListener(
+        "animationend",
+        handleAnimationEnd
+      );
+      mainContainerRef.current?.classList.add("fade");
+    }
+    // Add the event listener
+  };
+
   // Update pointers after index change
   useEffect(() => {
     fixPointer(rightPointerRef.current, arrayRefs.current[RightPointerIndex]);
@@ -90,8 +120,9 @@ const BinarySearch = (_props: Props) => {
 
   return (
     <>
-      <div className="main_component">
+      <div className="main_component" ref={mainContainerRef}>
         <div className="binary_main">
+          <p>Searching for: {searchVal}</p>
           <div
             className="binary_search_array"
             ref={arrayContainerRef}
@@ -107,14 +138,23 @@ const BinarySearch = (_props: Props) => {
               </div>
             ))}
           </div>
+          <div className="button_container">
+            <button
+              className="binary_next"
+              ref={nextButtonRef}
+              onClick={() => handleReturnClick()}
+            >
+              Quit
+            </button>
 
-          <button
-            className="binary_next"
-            ref={nextButtonRef}
-            onClick={() => handleNextClick()}
-          >
-            Next
-          </button>
+            <button
+              className="binary_next"
+              ref={nextButtonRef}
+              onClick={() => handleNextClick()}
+            >
+              Next
+            </button>
+          </div>
         </div>
 
         <div className="pointer" ref={leftPointerRef}>
